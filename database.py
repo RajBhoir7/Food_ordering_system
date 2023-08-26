3752e64a1f16c0d881c81ba51ce040cb36511ee3
Raj bhoir <bhoirraj872@gmail.com>
2023-08-27 00:08:12 +0530

    In Database file Unwanted comments Remove

--- database.py
+++ database.py
@@ -526,8 +526,7 @@ def Find_order_id_for_place(id,order_id):
         print("NOT FOUND")
 
 
-#x = find_order_id_for_place(12)
-#print(x)
+
 
 def insert_menu_items():
     sql = '''insert into menu(Item_Name,Price,Food_Type) values('Paneer Tikka',200,'Veg'),('Paneer Malai Tikka',220,'Veg'),('Paneer Garlic Tikka',225,'Veg'),('Tandoori Mashroom',190,'Veg'),('Paneer Tikka Roll',160,'Veg'),('Fresh Veggie Pizza',160,'Veg'),('Peppy Paneer Pizza',150,'Veg'),('Chicken Dominator Pizza',205,'Non-Veg'),('Checken Pepperoni Pizza',260,'Non-Veg'),('Creamy Pasta Pizza',120,'Non-Veg'),('Chiken Tikka',230,'Non-Veg'),('Mutton Tikka',360,'Non-Veg'),('Veg Loaded Pizza',140,'Veg'),('Veg Zinger Burger',80,'Veg'),('Water Bottle 1L',20,'Drink'),('Soda Bottle',25,'Drink'),('Red Bull',110,'Drink'),('Green Salad',80,'Veg');'''
